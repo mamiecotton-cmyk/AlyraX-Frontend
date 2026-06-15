151061eea7aeb72edc101c5dce078bab4ba29caa
mamiecotton-cmyk <mamiecotton-cmyk@users.noreply.github.com>
2026-06-14 19:15:32 -0700

    Lower Show Feet LoRA strength

--- app/api/generate-flux-selfie/route.ts
+++ app/api/generate-flux-selfie/route.ts
@@ -88,7 +88,7 @@ function buildFluxWorkflow({
   if (useNsfwLora) addLora('nsfw_flux.safetensors', nsfwLoraStrength);
   addLora(loraFile, loraStrength);
   if (useNsfwLora && refinementLoraFile && refinementStrength) addLora(refinementLoraFile, refinementStrength);
-  addLora('Show_Feet_-.safetensors', 0.45);
+  addLora('Show_Feet_-.safetensors', 0.3);
 
   const modelOutput = currentModel;
 
